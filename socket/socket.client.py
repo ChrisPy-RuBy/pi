60fd43d4c7e7537baa568810a8c151921967d034
Chris Woodall <christopherhwoodall@gmail.com>
2020-03-17 23:00:05 +0000

    see above

--- socket/socket.client.py
+++ socket/socket.client.py
@@ -1,15 +1,15 @@
 import socket
 
-HOST = ''
+HOST = '192.168.1.145'
 PORT = 12345
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
 while True:
-    command = raw_input("Enter command")
-    s.send(command)
+    command = input("Enter command: ")
+    s.send(command.encode())
     reply = s.recv(1024)
     if reply == "Terminate":
         break
-    print(reply)
+    print(reply.decode())
 
