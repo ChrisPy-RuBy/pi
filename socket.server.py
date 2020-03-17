e0da73755284cacbca125130319c517a13c3fbca
Chris Woodall <christopherhwoodall@gmail.com>
2020-03-17 22:27:49 +0000

    x

---
+++ socket.server.py
@@ -0,0 +1,36 @@
+import socket 
+
+HOST = '192.168.0.337' # server ip or hostname
+PORT = 12345
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("socket created")
+
+try:
+    s.bind((HOST, PORT))
+except socket.error:
+    print("Bind failed")
+
+s.listen(1)
+print("Socket awaiting messages")
+(conn, addr) = s.accept()
+print("Connected")
+
+while True:
+    data = conn.recv(1024)
+    print("I sent  message in response to: " + data)
+
+    # process message
+    if data == 'Hello':
+        reply = 'Hi, back!'
+    elif data == 'This is important':
+        reply = 'OK, fuck shit bum'
+
+    elif data == 'quit':
+        conn.send('Terminating')
+        break
+    else:
+        reply = 'Unknown command'
+
+    conn.send(reply)
+conn.close()
